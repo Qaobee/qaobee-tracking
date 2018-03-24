cc886c97d6ef660ad472c0c6a333de711b5d884f
Xavier MARIN <marin.xavier@gmail.com>
2018-03-24 17:17:26 +0100

    cleanup

--- src/components/next-event/next-event.ts
+++ src/components/next-event/next-event.ts
@@ -1,10 +1,8 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {EventsService} from "../../providers/api/events.service";
 import {AuthenticationService} from "../../providers/authentication.service";
 import {Storage} from "@ionic/storage";
 import {Utils} from "../../providers/utils";
-
-declare var google;
 
 /**
  * Generated class for the NextEventComponent component.
@@ -16,10 +14,7 @@ declare var google;
     selector: 'next-event',
     templateUrl: 'next-event.html'
 })
-export class NextEventComponent  implements AfterViewInit {
-
-    @ViewChild('mapelement', { read: ElementRef })
-    mapelement: ElementRef;
+export class NextEventComponent {
 
     user: any;
     nextEvent: any;
@@ -28,13 +23,9 @@ export class NextEventComponent  implements AfterViewInit {
                 private storage: Storage,
                 private authenticationService: AuthenticationService,
                 private utils: Utils) {
-    }
 
-    ngAfterViewInit(): void {
         this.user = this.authenticationService.user;
-        console.log(this.mapelement, document.getElementById("map"))
         this.storage.get('events').then(events => {
-            console.log(events);
             if (!events) {
                 this.eventsServices.getEvents(
                     this.authenticationService.meta.season.startDate,
@@ -57,19 +48,6 @@ export class NextEventComponent  implements AfterViewInit {
         let events = eventList.sort(this.utils.compareEvents);
         if(events.length > 0) {
             this.nextEvent = events[0];
-            this.loadMap();
         }
-    }
-    loadMap() {
-        let address = this.user.address;
-        console.log(this.mapelement)
-       /* let map = new google.maps.Map(this.mapelement.nativeElement, {
-            zoom: 4,
-            center: {lat: address.lat, lng: address.lng }
-        });
-        let marker = new google.maps.Marker({
-            position: {lat: address.lat, lng: address.lng },
-            map: map
-        });*/
     }
 }
